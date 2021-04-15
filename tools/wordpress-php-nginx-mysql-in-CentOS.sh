5de83269d48b63d9a71d8451f988372062acbb38
Jenkins <duocnh@vatgia.com>
2021-04-15 14:40:56 +0700

    ok

--- tools/wordpress-php-nginx-mysql-in-CentOS.sh
+++ tools/wordpress-php-nginx-mysql-in-CentOS.sh
@@ -122,3 +122,6 @@ CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '1';
 FLUSH PRIVILEGES;
 EXIT;
+
+firewall-cmd --add-port=3306/tcp --permanent
+firewall-cmd --reload
