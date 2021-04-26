68db5a880bdc180a69f883fc59baf33f5771cfef
Jenkins <duocnh@vatgia.com>
2021-04-26 13:51:05 +0700

    ok

--- linux command.sh
+++ linux command.sh
@@ -311,8 +311,21 @@ EOF
 
 # fn_crontab
 # set auto job
-  crontab
+  # list job
+    crontab -l
 
+  # crontab.guru
+    to know the time to set in crontab
+
+  # edit
+    crontab -l
+    40 16 * * * wget -O - https://bkpos.baokim.vn:9106/api/v1/cross-check/ten-day > /dev/null 2>&1
+    crontab -e
+    shift + P + G
+    - i 
+    edit
+    press esc
+    :wq! 
 # fn_dd
 # to know I/O speed
   dd if=./latest.tar.gz of=/tmp/latest.tar.gz
